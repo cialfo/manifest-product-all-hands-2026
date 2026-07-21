4d6d202d40c8eb5ed76bb3a5d4321c8d6d724ba8
DhruvCialfo <dhruv.pandey@cialfo.com.sg>
2026-07-21 11:24:42 +0530

    Remove period from University Profile Redefined title

--- stories-index.js
+++ stories-index.js
@@ -23,7 +23,7 @@ window.STORIES = [
   {
     id: "uniprofile",
     file: "story-02-uniprofile.html",
-    title: "University Profile. Redefined",
+    title: "University Profile Redefined",
     author: "Charles David",
     section: "Cialfo",
     dateAdded: "2026-07-20",
